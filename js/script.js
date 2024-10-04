8dc2d56fb626f6370aa8438bdcfe9b2c19a02dad
karan9186 <parmarkarans7486@gmail.com>
2024-10-04 22:02:16 +0530

    new cjanges

--- js/script.js
+++ js/script.js
@@ -25,3 +25,25 @@ $(document).ready(function () {
     },
   });
 });
+// for swipper
+
+var swipper = new Swiper(".mySwiper", {
+  loop: true,
+  autoplay: true,
+  autoplayTimeout: 1000,
+  autoplayHoverPause: true,
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 0,
+    depth: 300,
+    modifier: 1,
+    slideShadows: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
